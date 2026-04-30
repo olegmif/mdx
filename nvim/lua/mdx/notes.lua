9cc90b56c02399c2d66805617517e182305b3e53
Oleg Miftakhov <olegmif@gmail.com>
2026-05-01 00:29:24 +0500

    m3 step 3

--- nvim/lua/mdx/notes.lua
+++ nvim/lua/mdx/notes.lua
@@ -1,7 +1,26 @@
 local M = {}
 
 function M.list()
-	return nil
+	local clients = vim.lsp.get_clients({ name = "mdx" })
+	if #clients == 0 then
+		vim.notify("mdx: LSP client not attached", vim.log.levels.ERROR)
+		return nil
+	end
+
+	local response = clients[1]:request_sync("mdx/listNotes", vim.empty_dict(), 2000, 0)
+
+	if not response then
+		vim.notify("mdx: listNotes timed out", vim.log.levels.ERROR)
+		return nil
+	end
+
+	if response.err then
+		local msg = type(response.err) == "table" and response.err.message or tostring(response.err)
+		vim.notify("mdx: listNotes error: " .. msg, vim.log.levels.ERROR)
+		return nil
+	end
+
+	return response.result
 end
 
 return M
